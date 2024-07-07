d265b5082e4b41ad5da552428fedc19d26bd8a9b
kastastin <kostya242421@gmail.com>
2024-07-07 03:31:53 +0300

    initial project setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "E-Commerce",
+  description: "A complete e-commerce application with Next.js and Wix",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+const HomePage = () => {
+  return <div className="flex text-white">HomePage</div>;
+};
+
+export default HomePage;
