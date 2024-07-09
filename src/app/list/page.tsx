9192889886bf4db3cc0d99022f1c82498882a1c8
kastastin <kostya242421@gmail.com>
2024-07-09 09:06:57 +0300

    added Product List page with filters

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,5 +1,37 @@
+import Image from "next/image";
+
+import Filter from "@/components/Filter";
+import ProductList from "@/components/ProductList";
+
 const ListPage = () => {
-  return <div>ListPage</div>;
+  return (
+    <div className="relative px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+      {/* Campaign */}
+      <div className="hidden h-64 justify-between bg-pink-50 px-4 sm:flex">
+        <div className="flex w-2/3 flex-col items-center justify-center gap-8">
+          <h1 className="text-4xl font-semibold leading-[48px] text-gray-700">
+            Grab up to 50% off on
+            <br /> Selected Products
+          </h1>
+
+          <button className="w-max rounded-3xl bg-red-400 px-5 py-3 text-sm text-white">
+            Buy Now
+          </button>
+        </div>
+
+        <div className="relative w-1/3">
+          <Image src="/woman.png" alt="Woman" fill className="object-contain" />
+        </div>
+      </div>
+
+      {/* Filter */}
+      <Filter />
+
+      {/* Products */}
+      <h1 className="mt-12 text-xl font-semibold">Shoes For You!</h1>
+      <ProductList />
+    </div>
+  );
 };
 
 export default ListPage;
