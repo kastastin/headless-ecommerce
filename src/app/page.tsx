3a01b198647310b820864cbcddc43e4e9fb63812
kastastin <kostya242421@gmail.com>
2024-07-13 07:35:32 +0300

    added Suspense (fetching data from wix api) to ProductList component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,37 @@
+import { Suspense } from "react";
+
 import Slider from "@/components/Slider";
+import Skeleton from "@/components/Skeleton";
 import ProductList from "@/components/ProductList";
 import CategoryList from "@/components/CategoryList";
-import { wixClientServer } from "@/lib/wixClientServer";
 
-const HomePage = async () => {
-  const wixClient = await wixClientServer();
+const HomePage = () => (
+  <div>
+    <Slider />
 
-  const res = await wixClient.products.queryProducts().find();
+    <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+      <h1 className="text-2xl">Featured Products</h1>
 
-  console.log(res);
-
-  return (
-    <div>
-      <Slider />
-
-      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-        <h1 className="text-2xl">Featured Products</h1>
-        <ProductList />
-      </div>
-
-      <div className="mt-24">
-        <h1 className="mb-12 px-4 text-2xl md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-          Categories
-        </h1>
-        <CategoryList />
-      </div>
-
-      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
-        <h1 className="text-2xl">New Products</h1>
-        <ProductList />
-      </div>
+      <Suspense fallback={<Skeleton />}>
+        <ProductList
+          categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID!}
+          limit={4}
+        />
+      </Suspense>
     </div>
-  );
-};
+
+    <div className="mt-24">
+      <h1 className="mb-12 px-4 text-2xl md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+        Categories
+      </h1>
+      <CategoryList />
+    </div>
+
+    <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+      <h1 className="text-2xl">New Products</h1>
+      {/* <ProductList /> */}
+    </div>
+  </div>
+);
 
 export default HomePage;
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,144 +1,77 @@
 import Link from "next/link";
 import Image from "next/image";
+import { products } from "@wix/stores";
+import DOMPurify from "isomorphic-dompurify";
 
-const ProductList = () => {
+import { wixClientServer } from "@/lib/wixClientServer";
+
+interface ProductListProps {
+  categoryId: string;
+  limit?: number;
+}
+
+const DEFAULT_PRODUCTS_LIMIT = 20;
+
+const ProductList = async ({ categoryId, limit }: ProductListProps) => {
+  const wixClient = await wixClientServer();
+
+  const res = await wixClient.products
+    .queryProducts()
+    .eq("collectionIds", categoryId)
+    .limit(limit || DEFAULT_PRODUCTS_LIMIT)
+    .find();
+
   return (
     <div className="mt-12 flex flex-wrap justify-between gap-x-8 gap-y-16">
-      <Link
-        href="/test"
-        className="relative flex w-full flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-      >
-        <div className="relative h-80 w-full">
-          <Image
-            src="https://images.unsplash.com/photo-1719861032503-225fac307c59?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8"
-            alt="Product"
-            fill
-            sizes="25vw"
-            className="absolute z-10 rounded-md object-cover transition-opacity duration-500 ease-in-out hover:opacity-0"
-          />
+      {res.items.map((product: products.Product) => (
+        <Link
+          key={product._id}
+          href={`/${product.slug}`}
+          className="relative flex w-full flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+        >
+          <div className="relative h-80 w-full">
+            <Image
+              src={product.media?.mainMedia?.image?.url || "/product.png"}
+              alt="Product"
+              fill
+              sizes="25vw"
+              className="absolute z-10 rounded-md object-cover transition-opacity duration-500 ease-in-out hover:opacity-0"
+            />
 
-          <Image
-            src="https://images.unsplash.com/photo-1719917226565-d66c05937ba2?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8"
-            alt="Product"
-            fill
-            sizes="25vw"
-            className="absolute rounded-md object-cover"
-          />
-        </div>
+            {product.media?.items && (
+              <Image
+                src={product.media?.items[1]?.image?.url || "/product.png"}
+                alt="Product"
+                fill
+                sizes="25vw"
+                className="absolute rounded-md object-cover"
+              />
+            )}
+          </div>
 
-        <div className="flex justify-between">
-          <span className="font-medium">Product Name</span>
-          <span className="font-semibold">$49</span>
-        </div>
+          <div className="flex justify-between">
+            <span className="font-medium">{product.name}</span>
+            <span className="font-semibold">${product.priceData?.price}</span>
+          </div>
 
-        <div className="text-sm text-gray-500">Desctiption</div>
+          {product.additionalInfoSections && (
+            <div
+              className="text-sm text-gray-500"
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(
+                  product.additionalInfoSections.find(
+                    (section: any) => section.title === "shortDescription",
+                  )?.description || "No description",
+                ),
+              }}
+            />
+          )}
 
-        <button className="w-max rounded-2xl px-4 py-2 text-xs text-red-400 ring-1 ring-red-400 hover:bg-red-400 hover:text-white">
-          Add to Cart
-        </button>
-      </Link>
-
-      <Link
-        href="/test"
-        className="relative flex w-full flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-      >
-        <div className="relative h-80 w-full">
-          <Image
-            src="https://images.unsplash.com/photo-1719861032503-225fac307c59?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8"
-            alt="Product"
-            fill
-            sizes="25vw"
-            className="absolute z-10 rounded-md object-cover transition-opacity duration-500 ease-in-out hover:opacity-0"
-          />
-
-          <Image
-            src="https://images.unsplash.com/photo-1719917226565-d66c05937ba2?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8"
-            alt="Product"
-            fill
-            sizes="25vw"
-            className="absolute rounded-md object-cover"
-          />
-        </div>
-
-        <div className="flex justify-between">
-          <span className="font-medium">Product Name</span>
-          <span className="font-semibold">$49</span>
-        </div>
-
-        <div className="text-sm text-gray-500">Desctiption</div>
-
-        <button className="w-max rounded-2xl px-4 py-2 text-xs text-red-400 ring-1 ring-red-400 hover:bg-red-400 hover:text-white">
-          Add to Cart
-        </button>
-      </Link>
-
-      <Link
-        href="/test"
-        className="relative flex w-full flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-      >
-        <div className="relative h-80 w-full">
-          <Image
-            src="https://images.unsplash.com/photo-1719861032503-225fac307c59?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8"
-            alt="Product"
-            fill
-            sizes="25vw"
-            className="absolute z-10 rounded-md object-cover transition-opacity duration-500 ease-in-out hover:opacity-0"
-          />
-
-          <Image
-            src="https://images.unsplash.com/photo-1719917226565-d66c05937ba2?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8"
-            alt="Product"
-            fill
-            sizes="25vw"
-            className="absolute rounded-md object-cover"
-          />
-        </div>
-
-        <div className="flex justify-between">
-          <span className="font-medium">Product Name</span>
-          <span className="font-semibold">$49</span>
-        </div>
-
-        <div className="text-sm text-gray-500">Desctiption</div>
-
-        <button className="w-max rounded-2xl px-4 py-2 text-xs text-red-400 ring-1 ring-red-400 hover:bg-red-400 hover:text-white">
-          Add to Cart
-        </button>
-      </Link>
-
-      <Link
-        href="/test"
-        className="relative flex w-full flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-      >
-        <div className="relative h-80 w-full">
-          <Image
-            src="https://images.unsplash.com/photo-1719861032503-225fac307c59?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8"
-            alt="Product"
-            fill
-            sizes="25vw"
-            className="absolute z-10 rounded-md object-cover transition-opacity duration-500 ease-in-out hover:opacity-0"
-          />
-
-          <Image
-            src="https://images.unsplash.com/photo-1719917226565-d66c05937ba2?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8"
-            alt="Product"
-            fill
-            sizes="25vw"
-            className="absolute rounded-md object-cover"
-          />
-        </div>
-
-        <div className="flex justify-between">
-          <span className="font-medium">Product Name</span>
-          <span className="font-semibold">$49</span>
-        </div>
-
-        <div className="text-sm text-gray-500">Desctiption</div>
-
-        <button className="w-max rounded-2xl px-4 py-2 text-xs text-red-400 ring-1 ring-red-400 hover:bg-red-400 hover:text-white">
-          Add to Cart
-        </button>
-      </Link>
+          <button className="w-max rounded-2xl px-4 py-2 text-xs text-red-400 ring-1 ring-red-400 hover:bg-red-400 hover:text-white">
+            Add to Cart
+          </button>
+        </Link>
+      ))}
     </div>
   );
 };
