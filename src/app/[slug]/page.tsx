9de2d3798a9ddffdc3d676515a9853dd86ad6fa5
kastastin <kostya242421@gmail.com>
2024-07-13 13:25:23 +0300

    added fetch single product pages

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,32 +1,52 @@
+import { notFound } from "next/navigation";
+
 import Add from "@/components/Add";
+import { wixClientServer } from "@/lib/wixClientServer";
 import ProductImages from "@/components/ProductImages";
 import CustomProducts from "@/components/CustomProducts";
 
-const SinglePage = () => {
+const SinglePage = async ({ params }: { params: { slug: string } }) => {
+  const wixClient = await wixClientServer();
+
+  const res = await wixClient.products
+    .queryProducts()
+    .eq("slug", params.slug)
+    .find();
+
+  if (!res.items[0]) return notFound();
+
+  const product = res.items[0];
+
   return (
     <div className="relative flex flex-col gap-16 px-4 md:px-8 lg:flex-row lg:px-16 xl:px-32 2xl:px-64">
       {/* Image container */}
       <div className="top-20 h-max w-full lg:sticky lg:w-1/2">
-        <ProductImages />
+        <ProductImages items={product.media?.items} />
       </div>
 
       {/* Text container */}
       <div className="flex w-full flex-col gap-6 lg:w-1/2">
-        <h1 className="text-4xl font-medium">Product Name</h1>
+        <h1 className="text-4xl font-medium">{product.name}</h1>
 
-        <p className="text-gray-500">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet
-          maxime itaque, nobis rerum repellat exercitationem! Nihil reiciendis
-          deleniti alias cupiditate minus aspernatur dolore eaque adipisci,
-          voluptate sint est, animi sapiente.
-        </p>
+        <p className="text-gray-500">{product.description}</p>
 
         <div className="h-[2px] bg-gray-100" />
 
-        <div className="flex items-center gap-4">
-          <h3 className="text-xl text-gray-500 line-through">$59</h3>
-          <h2 className="text-2xl font-medium">$49</h2>
-        </div>
+        {product.priceData?.price === product.priceData?.discountedPrice ? (
+          <h2 className="text-xl text-gray-500 line-through">
+            ${product.priceData?.price}
+          </h2>
+        ) : (
+          <div className="flex items-center gap-4">
+            <h3 className="text-xl text-gray-500 line-through">
+              ${product.priceData?.price}
+            </h3>
+
+            <h2 className="text-2xl font-medium">
+              ${product.priceData?.discountedPrice}
+            </h2>
+          </div>
+        )}
 
         <div className="h-[2px] bg-gray-100" />
 
@@ -35,38 +55,13 @@ const SinglePage = () => {
 
         <div className="h-[2px] bg-gray-100" />
 
-        <div className="text-sm">
-          <h4 className="mb-4 font-medium">Title</h4>
+        {product.additionalInfoSections?.map((section: any) => (
+          <div key={section.title} className="text-sm">
+            <h4 className="mb-4 font-medium">{section.title}</h4>
 
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Repellendus, reprehenderit culpa! Magni beatae temporibus et placeat
-            nulla fugit distinctio reprehenderit sequi, iusto optio aspernatur
-            mollitia quis eaque incidunt accusamus non!
-          </p>
-        </div>
-
-        <div className="text-sm">
-          <h4 className="mb-4 font-medium">Title</h4>
-
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Repellendus, reprehenderit culpa! Magni beatae temporibus et placeat
-            nulla fugit distinctio reprehenderit sequi, iusto optio aspernatur
-            mollitia quis eaque incidunt accusamus non!
-          </p>
-        </div>
-
-        <div className="text-sm">
-          <h4 className="mb-4 font-medium">Title</h4>
-
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Repellendus, reprehenderit culpa! Magni beatae temporibus et placeat
-            nulla fugit distinctio reprehenderit sequi, iusto optio aspernatur
-            mollitia quis eaque incidunt accusamus non!
-          </p>
-        </div>
+            <p>{section.description}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -3,33 +3,14 @@
 import { useState } from "react";
 import Image from "next/image";
 
-const images = [
-  {
-    id: 1,
-    url: "https://images.unsplash.com/photo-1720299967200-9d6beef0260b?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3NXx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    id: 2,
-    url: "https://images.unsplash.com/photo-1718016675738-7772815af223?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2OHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    id: 3,
-    url: "https://images.unsplash.com/photo-1720170717116-3b3180ed20c9?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw5NHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    id: 4,
-    url: "https://plus.unsplash.com/premium_photo-1671512497536-e20cc1084ea7?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMjl8fHxlbnwwfHx8fHw%3D",
-  },
-];
-
-const ProductImages = () => {
+const ProductImages = ({ items }: { items: any }) => {
   const [imageIndex, setImageIndex] = useState(0);
 
   return (
     <div>
       <div className="relative h-[500px]">
         <Image
-          src={images[imageIndex].url}
+          src={items[imageIndex].image?.url}
           alt="Product"
           fill
           sizes="50vw"
@@ -38,14 +19,14 @@ const ProductImages = () => {
       </div>
 
       <div className="mt-4 flex justify-between gap-4">
-        {images.map((image, i) => (
+        {items.map((item: any, i: number) => (
           <div
-            key={image.id}
+            key={item._id}
             className="relative mt-4 h-32 w-1/4 cursor-pointer gap-4"
             onClick={() => setImageIndex(i)}
           >
             <Image
-              src={image.url}
+              src={item.image?.url}
               alt="Product"
               fill
               sizes="30vw"
